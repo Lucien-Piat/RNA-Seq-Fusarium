a37ba0bc7caa2519a9a11dc9b8343ebeabedbcc2
malani86 <134076665+malani86@users.noreply.github.com>
2024-05-03 21:57:15 +0200

    Add files via upload

---
+++ align.sh
@@ -0,0 +1,60 @@
+#!/bin/bash
+
+# Load configurations from JSON
+CONFIG_FILE="./rna_seq_config.json"
+
+# Use jq to extract paths
+TRIMMED_OUTPUT_DIR=$(jq -r '.scripts[] | select(.name=="align.sh") | .paths.TRIMMED_OUTPUT_DIR' "$CONFIG_FILE")
+GENOME_DIR=$(jq -r '.scripts[] | select(.name=="align.sh") | .paths.GENOME_DIR' "$CONFIG_FILE")
+OUTPUT_DIR=$(jq -r '.scripts[] | select(.name=="align.sh") | .paths.OUTPUT_DIR' "$CONFIG_FILE")
+
+# Path to the BWA-MEM2 executable
+BWA_MEM2="$HOME/bwa-mem2/bwa-mem2/bwa-mem2.avx2"
+
+# Lists of reference genomes
+GENOMES=("Fgraminearum_Genome.fasta" "Fverticillioides_Genome.fasta")
+
+# Check if the output directory exists and create it if not
+if [ ! -d "$OUTPUT_DIR" ]; then
+  mkdir -p "$OUTPUT_DIR"
+  echo "Created output directory: $OUTPUT_DIR"
+fi
+
+# Function to check if genome is indexed
+is_indexed() {
+  local genome_path="$1"
+  local expected_files=("bwt" "pac" "ann" "amb" "sa")
+
+  for ext in "${expected_files[@]}"; do
+    if [ ! -f "${genome_path}.${ext}" ]; then
+      return 1  # Not indexed
+    fi
+  done
+  return 0  # Indexed
+}
+
+# Index genomes if not already indexed
+for genome in "${GENOMES[@]}"; do
+  genome_path="$GENOME_DIR/$genome"
+  if ! is_indexed "$genome_path"; then
+    echo "Indexing $genome"
+    $BWA_MEM2 index "$genome_path"
+  else
+    echo "$genome is already indexed"
+  fi
+done
+
+# Define the read files from TRIMMED_OUTPUT_DIR
+READS=($(ls "$TRIMMED_OUTPUT_DIR"/*.fastq.gz))
+
+# Loop through all genomes and reads for alignment
+for genome in "${GENOMES[@]}"; do
+  for read in "${READS[@]}"; do
+    echo "Aligning $read to $genome"
+    output_path="$OUTPUT_DIR/${genome}_$(basename "$read" .fastq.gz).sam"
+    command="$BWA_MEM2 mem \"$genome_path\" \"$read\" > \"$output_path\""
+    eval "$command"
+  done
+done
+
+echo "Alignment process complete."
